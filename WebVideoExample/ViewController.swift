7b58e0704e337652358ce64e640cf80fbed3dbe5
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-19 13:06:39 -0500

    Initial Commit

---
+++ WebVideoExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebVideoExample
+//
+//  Created by jose sanchez on 8/19/20.
+//  Copyright © 2020 jose sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WebVideoExampleTests/WebVideoExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  WebVideoExampleTests.swift
+//  WebVideoExampleTests
+//
+//  Created by jose sanchez on 8/19/20.
+//  Copyright © 2020 jose sanchez. All rights reserved.
+//
+
+import XCTest
+@testable import WebVideoExample
+
+class WebVideoExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
