e55c5cf8b3607e9190e9b90585a05847947820b8
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-19 13:31:55 -0500

    switching videos added

--- WebVideoExample/ViewController.swift
+++ WebVideoExample/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import WebKit
+import AVKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mainWebView: WKWebView!
+    
+    let videoURLs = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "https://www.youtube.com/watch?v=h6b0_MEWpW8"
+    ]
+    
+    @IBAction func onLoadVideo1Pressed(_ sender: Any) {
+        print("Video 1!")
+        mainWebView.load(URLRequest(url: URL(string: videoURLs[0])!))
 
+    }
+    
+    @IBAction func onLoadVideo2Pressed(_ sender: Any) {
+        print("Video 2!")
+        mainWebView.load(URLRequest(url: URL(string: videoURLs[1])!))
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainWebView.load(URLRequest(url: URL(string: videoURLs[0])!))
     }
 
 
